e0f959b23287fbda1b3ce3aa4f15e50380ae9f8e
guole <guole.fun@qq.com>
2024-11-21 16:21:14 +0800

    🏖️ API更新于 2024年11月21日 16:21:14

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,11 +3,8 @@ import { html } from "hono/html";
 import Layout from "./Layout.js";
 import fs from 'fs/promises';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const packagePath = path.resolve(__dirname, 'package.json');
+const packagePath = path.resolve('./', 'package.json');
 const packageJson = JSON.parse(await fs.readFile(packagePath, 'utf-8'));
 const version = packageJson.version;
 
