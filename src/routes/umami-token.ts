3b50fc2c7f5237d0ba13c5ae268ec5bfa373daa4
guole <guole.fun@qq.com>
2025-02-25 11:49:32 +0800

    🏖️ API更新于 2025-02-25 11:49:32

--- src/routes/umami-token.ts
+++ src/routes/umami-token.ts
@@ -7,6 +7,10 @@ const name = process.env.UMAMI_USER_NAME || '';
 const password = process.env.UMAMI_PASSWORD || '';
 
 export const handleRoute = async (c: ListContext, noCache: boolean) => {
+  if (!name || !password) {
+    throw new HttpError(401, `Umami 用户名或密码不正确！`);
+  }
+
   const url = `https://umami.guole.fun/api/auth/login`;
   const result = await post({
     url,
@@ -18,9 +22,6 @@ export const handleRoute = async (c: ListContext, noCache: boolean) => {
     ttl
   });
   const data = result.data;
-  if (!result.data || !data.token) {
-    throw new HttpError(401, `Umami 用户名或密码不正确！`);
-  }
   const routeData: OtherData = {
     ...data,
   };
