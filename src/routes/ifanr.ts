4d08b9cf145dcfbf3cb577ca77879efcde827e1f
guole <guole.fun@qq.com>
2025-02-27 16:45:29 +0800

    🏖️ API更新于 2025-02-27 16:45:29

--- src/routes/ifanr.ts
+++ src/routes/ifanr.ts
@@ -21,12 +21,13 @@ const getList = async (noCache: boolean) => {
   const url = "https://sso.ifanr.com/api/v5/wp/buzz/?limit=20&offset=0";
   const result = await get({ url, noCache });
   const list = result.data.objects;
+  //console.log(list)
   return {
     ...result,
     data: list.map((v: RouterType["ifanr"]) => ({
       id: v.id,
       title: v.post_title,
-      desc: v.post_content,
+      desc: cleanPostContent(v.post_content),
       timestamp: getTime(v.created_at),
       hot: v.like_count || v.comment_count,
       url: v.buzz_original_url || `https://www.ifanr.com/${v.post_id}`,
@@ -34,3 +35,14 @@ const getList = async (noCache: boolean) => {
     })),
   };
 };
+
+const cleanPostContent = (postContent: string, maxWords: number = 500): string => {
+  const text = postContent.replace(/<[^>]*>/g, ''); // 去除 HTML 标签
+  const cleanedText = text.replace(/\n/g, ' ').replace(/\s+/g, ' ').trim(); // 去除换行符和多余空格
+  const words = cleanedText.split(' '); // 按空格分割成单词
+
+  if (words.length <= maxWords) {
+      return cleanedText;
+  }
+  return words.slice(0, maxWords).join(' ') + '...';
+}
